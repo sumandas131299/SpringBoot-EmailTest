3f007c8268dfba701ab0a536534ca5575c47c7ae
suman <sumandas131299@gmail.com>
2024-07-30 06:49:39 -0700

    All Attachments and html to pdf conversion and image as attachment

--- src/main/java/com/AleefWebSolution_EmailTesting/emailTest/Controller/MainController.java
+++ src/main/java/com/AleefWebSolution_EmailTesting/emailTest/Controller/MainController.java
@@ -4,6 +4,10 @@ import com.AleefWebSolution_EmailTesting.emailTest.Model.email;
 import com.AleefWebSolution_EmailTesting.emailTest.Service.ServiceImp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,19 +24,57 @@ ServiceImp service;
     @PostMapping("/sendEmail")
     public String sendEmail(@RequestBody email Email){
          this.service.sendEmail(Email);
-         return  "Mail Send Sucessfully";
+         return  "Mail Send Successfully";
     }
 
-    @PostMapping("/uploadOnlyEmail")
+    @PostMapping("/uploadCsvMime")
     public String uploadEmail(@RequestParam("file") MultipartFile file) throws IOException {
         this.service.uploadEmail(file);
-        return "Mails sucessfully";
+        return "Mails successfully";
     }
 
-    @PostMapping("/uploadWithoutBody-Email")
+    @PostMapping("/uploadCsvWithoutBodyMime")
     public String uploadWithoutBody(@RequestParam("csv")MultipartFile file ,@RequestParam("sub")String sub,@RequestParam("body")String body ) throws IOException {
     this.service.uploadWithoutBody(file , sub,body);
-    return "Mail sent sucessfull";
+    return "Mail sent successfully";
     }
+
+    @PostMapping("/uploadCsvSmtp")
+    public String uploadWithSmtp(@RequestParam("file") MultipartFile file) throws IOException {
+        this.service.uploadWithSmtp(file);
+        return "Mail sent successfully";
+
+    }
+
+    @PostMapping("/uploadCsvWithoutBodySmtp")
+    public String uploadWithoutBodySmtp(@RequestParam("csv")MultipartFile file , @RequestParam("sub")String sub,@RequestParam("body")String body) throws IOException{
+        this.service.uploadWithoutBodySmtp(file,sub,body);
+        return "Mail sent successful";
+    }
+
+    @PostMapping("/uploadCsvWithImage")
+    public String uploadCsvWithImage(@RequestParam("csv") MultipartFile file , @RequestParam("img") MultipartFile image) throws IOException {
+        this.service.uploadCsvWithImage(file,image);
+        return "Mail sent with attachment";
+
+    }
+
+    @PostMapping("/convertHtmlToPdf")
+    public ResponseEntity<byte[]>   convertHtmlToPdf(@RequestBody String html){
+        try{
+            byte[] pdf = this.service.convertHtmlToPdf(html);
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"document.pdf\"").contentType(MediaType.APPLICATION_PDF).body(pdf);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+
+        }
+
+    @PostMapping("/uploadHtmlToPdfAttachment")
+    public ResponseEntity htmlToPdfAttachment(@RequestParam ("csv") MultipartFile csv , @RequestParam ("html")MultipartFile html) throws IOException {
+        this.service.htmlToPdfAttachment(csv,html);
+        return ResponseEntity.ok().body("mail sent with pdf attachment");
+    }
+
 
 }
--- src/main/java/com/AleefWebSolution_EmailTesting/emailTest/Service/ServiceImp.java
+++ src/main/java/com/AleefWebSolution_EmailTesting/emailTest/Service/ServiceImp.java
@@ -4,6 +4,7 @@ import com.AleefWebSolution_EmailTesting.emailTest.Model.email;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.util.ByteArrayDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,14 +14,16 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import  org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.sql.DataSource;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.io.*;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @Service
@@ -40,7 +43,12 @@ public class ServiceImp {
 
             // SimpleMailMessage actualMail = new SimpleMailMessage();
             actualMail.setFrom(fromEmail);
-            actualMail.setTo(Email.getTo());
+
+            @NotNull(message = "email is required")
+            @Email(message = "give a valid email address")
+            String to = Email.getTo();
+
+            actualMail.setTo(to);
             actualMail.setSubject(Email.getSubject());
             actualMail.setText(Email.getMessage());
             javaMailSender.send( message);
@@ -55,7 +63,7 @@ public class ServiceImp {
 
     }
 
-
+//    This method contain subject and body into the csv file and in MIME
 
     public void uploadEmail(MultipartFile file) throws IOException {
 
@@ -66,7 +74,12 @@ public class ServiceImp {
         while((line=br.readLine())!=null){
             String[] s= line.split(",");
             email Email = new email();
-            Email.setTo(s[0]);
+
+            @NotNull(message = "email is required")
+            @Email(message = "give a valid email address")
+            String to = s[0];
+
+            Email.setTo(to);
             Email.setSubject(s[1]);
             Email.setMessage(s[2]);
            // System.out.println(s[0]);
@@ -99,6 +112,8 @@ public class ServiceImp {
 
     }
 
+    //    This method doesn't contain subject and body into the csv file and in MIME
+
     public void uploadWithoutBody(MultipartFile file, String sub ,String body) throws IOException {
 
         InputStream is = file.getInputStream();
@@ -108,7 +123,14 @@ public class ServiceImp {
         while((line=br.readLine())!=null){
             String[] s= line.split(",");
             email Email = new email();
-            Email.setTo(s[0]);
+
+            @NotNull(message = "email is required")
+            @Email(message = "give a valid email address")
+            String to = s[0];
+
+            Email.setTo(to);
+
+
             Email.setSubject((String) sub);
             Email.setMessage((String) body);
             // System.out.println(s[0]);
@@ -133,7 +155,7 @@ public class ServiceImp {
 
 
             }catch (Exception e){
-                System.out.println("error in UploadOnlyEmail sending :: ServiceImp :: " + e );
+                System.out.println("error in uploadWithoutBody  :: ServiceImp :: " + e );
                 e.printStackTrace();
             }
             index++;
@@ -142,5 +164,230 @@ public class ServiceImp {
 
     }
 
+  //  This method contain subject and body into the csv file and in SMTP
+    public void uploadWithSmtp(MultipartFile file) throws IOException {
+        InputStream is = file.getInputStream();
+        BufferedReader br  = new BufferedReader(new InputStreamReader(is ,"UTF-8"));
+        String line = "";
+        List<email> list =new ArrayList();
+        while((line= br.readLine())!=null){
+            String[] s = line.split(",");
+            email Email = new email();
 
+            @NotNull(message = "email cannot be null")
+            @Email(message = "provide proper email address")
+             String to =s[0];
+
+            Email.setTo(to);
+            Email.setSubject(s[1]);
+            Email.setMessage(s[2]);
+            list.add(Email);
+
+        }
+
+        int index = 1;
+        while(index<list.size()){
+            System.out.println(list.get(index).getSubject());
+            System.out.println(list.size());
+
+            try{
+                // MimeMessage message = javaMailSender.createMimeMessage();
+                // MimeMessageHelper actualMail = new MimeMessageHelper(message);
+
+                SimpleMailMessage actualMail = new SimpleMailMessage();
+                actualMail.setFrom(fromEmail);
+                actualMail.setTo(list.get(index).getTo());
+                actualMail.setSubject(list.get(index).getSubject());
+                actualMail.setText(list.get(index).getMessage());
+                javaMailSender.send( actualMail);
+
+
+            }catch (Exception e){
+                System.out.println("error in UploadWithSmtp :: ServiceImp :: " + e );
+                e.printStackTrace();
+            }
+            index++;
+        }
+
+    }
+
+    //    This method doesn't contain subject and body into the csv file and in SMTP
+
+    public void uploadWithoutBodySmtp(MultipartFile file, String sub, String body) throws IOException {
+        InputStream is= file.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+        String line ="";
+        List<email> emailList=new ArrayList();
+        while((line=br.readLine())!=null){
+            String[] s= line.split(",");
+            email Email = new email();
+
+            @NotNull(message = "email is required")
+            @Email(message = "give a valid email address")
+            String to = s[0];
+
+            Email.setTo(to);
+
+
+            Email.setSubject((String) sub);
+            Email.setMessage((String) body);
+            // System.out.println(s[0]);
+            emailList.add(Email);
+        }
+
+        int index = 1;
+        while(index<emailList.size()){
+            System.out.println(emailList.get(index).getSubject());
+            System.out.println(emailList.size());
+
+            try{
+                //   MimeMessage message = javaMailSender.createMimeMessage();
+                //  MimeMessageHelper actualMail = new MimeMessageHelper(message);
+
+                SimpleMailMessage actualMail = new SimpleMailMessage();
+                actualMail.setFrom(fromEmail);
+                actualMail.setTo(emailList.get(index).getTo());
+                actualMail.setSubject(emailList.get(index).getSubject());
+                actualMail.setText(emailList.get(index).getMessage());
+                javaMailSender.send( actualMail);
+
+
+            }catch (Exception e){
+                System.out.println("error in uploadWithoutBodySmtp  :: ServiceImp :: " + e );
+                e.printStackTrace();
+            }
+            index++;
+        }
+    }
+
+    //    This method contain subject and body into the csv file and in MIME with Attachment file as Image
+
+    public void uploadCsvWithImage(MultipartFile file, MultipartFile image) throws IOException {
+
+            InputStream is = file.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            String line ="";
+            List<email> list=new ArrayList();
+            while((line=br.readLine())!=null){
+                String[] s= line.split(",");
+                email Email = new email();
+
+                @NotNull(message = "email is required")
+                @Email(message = "give a valid email address")
+                String to = s[0];
+
+                Email.setTo(to);
+                Email.setSubject(s[1]);
+                Email.setMessage(s[2]);
+                // System.out.println(s[0]);
+                list.add(Email);
+            }
+
+            int index = 1;
+            while(index<list.size()){
+                System.out.println(list.get(index).getSubject());
+                System.out.println(list.size());
+
+                try{
+                    MimeMessage message = javaMailSender.createMimeMessage();
+                    MimeMessageHelper actualMail = new MimeMessageHelper(message,true);
+
+                    // SimpleMailMessage actualMail = new SimpleMailMessage();
+                    actualMail.setFrom(fromEmail);
+                    actualMail.setTo(list.get(index).getTo());
+                    actualMail.setSubject(list.get(index).getSubject());
+                    actualMail.setText(list.get(index).getMessage());
+                   //if(image!=null && !image.isEmpty())
+                       actualMail.addAttachment((Objects.requireNonNull(image.getOriginalFilename())), image);
+                    javaMailSender.send( message);
+
+
+                }catch (Exception e){
+                    System.out.println("error in UploadCsvWithAttachment :: ServiceImp :: " + e );
+                    e.printStackTrace();
+                }
+                index++;
+            }
+    }
+
+    //    This method convert html to pdf and send it back to download;
+    public byte[] convertHtmlToPdf(String html) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(os);
+        return os.toByteArray();
+    }
+
+    //    This method convert html to pdf and send it with mail as attachment;
+
+    public void htmlToPdfAttachment(MultipartFile csv,@NotNull MultipartFile html) throws IOException {
+
+        //html multipartFile converted into string
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(html.getInputStream(),"UTF-8"));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while((line=br.readLine())!=null){
+            sb.append(line);
+        }
+        //string to pdf
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ITextRenderer render = new ITextRenderer();
+        render.setDocumentFromString(sb.toString());
+        render.layout();
+        render.createPDF(os);
+        byte[] pdf = os.toByteArray();
+
+        InputStream is = csv.getInputStream();
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(is ,"UTF-8"));
+        String li;
+        List<email> list = new ArrayList<>();
+        while((li=buffer.readLine())!=null){
+            String [] s = li.split(",");
+            email Email = new email();
+
+            @Email(message = "Enter valid message")
+            @NotNull(message = "Email cannot be null")
+            String to = s[0];
+
+            Email.setTo(to);
+            Email.setSubject(s[1]);
+            Email.setMessage(s[2]);
+
+            list.add(Email);
+
+        }
+
+        int index = 1;
+        while(index<list.size()){
+            System.out.println(list.get(index).getSubject());
+            System.out.println(list.size());
+
+            try{
+                MimeMessage message = javaMailSender.createMimeMessage();
+                MimeMessageHelper actualMail = new MimeMessageHelper(message,true);
+
+                // SimpleMailMessage actualMail = new SimpleMailMessage();
+                actualMail.setFrom(fromEmail);
+                actualMail.setTo(list.get(index).getTo());
+                actualMail.setSubject(list.get(index).getSubject());
+                actualMail.setText(list.get(index).getMessage());
+                //if(image!=null && !image.isEmpty())
+
+               // DataSource pdfData = (DataSource) new ByteArrayDataSource(pdf , "application/pdf");
+                actualMail.addAttachment("document.pdf", new ByteArrayDataSource(pdf , "application/pdf"));
+                javaMailSender.send( message);
+
+
+            }catch (Exception e){
+                System.out.println("error in UploadCsvWithAttachment :: ServiceImp :: " + e );
+                e.printStackTrace();
+            }
+            index++;
+        }
+
+    }
 }
